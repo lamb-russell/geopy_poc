e8670f9c5454f01bf3e8b46248bf80348983164c
RL <79713964+lamb-russell@users.noreply.github.com>
2021-04-06 14:55:20 -0400

    add sample address file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # geopy_poc
  test geopy mapbox geocoding api
+
+process addresses stored as lines in a csv file.  return list of tuples.  
+
+list of addresses obtained here
+https://gist.github.com/HeroicEric/1102788
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from geopy.geocoders import MapBox
 from geopy.extra.rate_limiter import RateLimiter
 
-ENV_VAR="MAPBOX_API_TOKEN"
+ENV_VAR = "MAPBOX_API_TOKEN"
 
 
 def get_token():
@@ -17,41 +17,47 @@ def get_token():
         return None
     return api_key
 
-def process_rows(file_path, start_row=None, end_row=None, one_result=True):
+
+def process_rows(file_path, start_row=None, end_row=None):
     """
     process rows in file
     :param file_path: path to CSV file
     :param start_row: starting row number (1 based)
     :param end_row: ending row number (1 based)
-    :param one_result: if True, return a single result per address.  if False, return a list of locations.
     :return: list of tuples with results.  first entry is input, second entry is output.
     """
-    api_token = get_token()
-    geolocator = MapBox(api_token)
-
-    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=0.2)
-
-    rowcount = 0
-    results = [] # store results as list
-    with open(file_path, newline="") as f: # open csv file
+    rowcount: int = 0
+    inputs = []  # parse inputs
+    with open(file_path, newline="") as f:  # open csv file
         reader = csv.reader(f)
         for row in reader:
             rowcount = rowcount + 1
-            old_address = row[0] #assumes one address per line
-            if start_row and rowcount < start_row: #skip rows preceding start parameter
+            if start_row and rowcount < start_row:  # skip rows preceding start parameter
                 continue
-            if end_row and rowcount > end_row: # stop when we exceed end param
+            if end_row and rowcount > end_row:  # stop when we exceed end param
                 break
-            new_address = geocode(old_address, exactly_one=one_result)
-            results.append((old_address,new_address))
+            old_address = row[0]  # assumes one address per line
+            inputs.append(old_address)
+
+    return geocode_addresses(inputs)
+
+def geocode_addresses(inputs, one_result=True):
+    """
+    geocode list of addresses
+    :param inputs: list of addresses to geocode
+    :param one_result: if True, return a single result per address.  if False, return a list of locations.
+    """
+    api_token = get_token()
+    geolocator = MapBox(api_token)
+    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=0.2)
+    results = []  # store results as list
+    for i in inputs:
+        new_address = geocode(i, exactly_one=one_result)
+        results.append((i, new_address))
 
     return results
 
 
-if __name__=="__main__":
-    result = process_rows("5k_raw_address.csv", 10, 15)
+if __name__ == "__main__":
+    result = process_rows("sample_addresses.txt", 10, 15)
     print(result)
-
-
-
-
